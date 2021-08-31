f057fbf590f5e2684b9107e99bd83ece7dd147d4
jjones254 <justinjones254@gmail.com>
2021-08-31 18:59:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,56 +3,72 @@ Treehouse Techdegree:
 Project 4 - Random Quote Generator
 *************************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community
 
-/*** 
- * `quotes` array 
-***/
+/** 
+  This is an array of objects for storing the quote, source, citation and year values.
+**/
 const quotes = [
   {
-    quote: `"The people who are crazy enough to think they can change the world are the ones who do."`,
+    quote: `The people who are crazy enough to think they can change the world are the ones who do.`,
     source: "Steve Jobs",
     year: 2013
   },
   {
-    quote: `"Don't kill what you hate, save what you love."`,
+    quote: `Don't kill what you hate, save what you love.`,
     source: "Elon Musk",
     citation: "Twitter"
   },
   {
-    quote: `"Be the best, not the first."`,
+    quote: `Be the best, not the first.`,
     source: "Tim Cook"
   },
   {
-    quote:`"I am not in competition with anyone but myself. My goal is to improve myself continuously."`,
+    quote:`I am not in competition with anyone but myself. My goal is to improve myself continuously.`,
     source: "Bill Gates"
   },
   {
-    quote: `"The biggest risk is not taking any risk."`,
+    quote: `The biggest risk is not taking any risk.`,
     source: "Mark Zuckerberg"
   }
 ];
 
-console.log(quotes);
 
 
-/***
- * `getRandomQuote` function
-***/
+/**
+  Returns a random object from the 'quotes' array.
+**/
+function getRandomQuote() {
+  
+  const random = Math.floor(Math.random() * quotes.length);
+ 
+  const quote = quotes[random]
+
+  return quote;
+}
 
 
 
-/***
- * `printQuote` function
-***/
+/**
+ Calls the getRandomQuote function and updates index.html with the values of the random quote.
+**/
+function printQuote() {
+  
+  const randomQuote = getRandomQuote();
+  
+  let quoteHtml = `<p class="quote">${randomQuote.quote}</p><p class="source">${randomQuote.source}`
+  if (randomQuote.citation) {
+    quoteHtml += `<span class="citation">${randomQuote.citation}</span>`;
+  }
+  if (randomQuote.year) {
+    quoteHtml += `<span class="year">${randomQuote.year}</span>`;
+  }
+  `</p>`;
+  
+  document.getElementById('quote-box').innerHTML = quoteHtml;
+}
 
 
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
+/**
+  Click event listener for the print quote button.
+**/
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
